917e690412d1947f7a7d5c844f675836565ce74b
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-15 20:29:33 -0300

    feat: submit handle function

--- src/App.js
+++ src/App.js
@@ -18,9 +18,13 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form(e) {
+  function handleSubmit() {
+    e.preventDefault();
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
