03f250bd09b63f76bbcbc778becec28e34a06573
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-17 17:22:14 -0300

    feat: states of form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const intialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,21 +21,31 @@ function Logo() {
 }
 
 function Form(e) {
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+
   function handleSubmit() {
     e.preventDefault();
+
+    if(!description) return
+
+    const newItem = {description, quantity, packed: false, id: Date.now()}
+
+    setDescription("")
+    setQuantity(1)
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={e => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
       <button>Add</button>
     </form>
   );
