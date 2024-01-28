1d0d73afbbfd59038c08e71adbd26200be79a03d
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-28 15:56:49 -0300

    feat: stats footer state management

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -110,10 +110,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list! 🚀</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer>
-      <em>🧳 You have X items on your list, and you already packed X (X%).</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go! ✈"
+          : `🧳 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%).`}
+      </em>
     </footer>
   );
 }
