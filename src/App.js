78c08a03aaaffbab45b1c5ca9f36ae0c3f025ad3
Lucas Scommegna <scommegnal@hotmail.com>
2024-01-28 16:12:41 -0300

    feat: clear list button

--- src/App.js
+++ src/App.js
@@ -19,11 +19,20 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleItems} />
       <PackingList
+        onClearList={handleClearList}
         onToggleItem={handleToggleItem}
         items={items}
         onDeleteItem={handleDeleteItem}
@@ -78,7 +87,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
@@ -113,6 +122,8 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+
+        <button onClick={onClearList}>Clear List</button>
       </div>
     </div>
   );
